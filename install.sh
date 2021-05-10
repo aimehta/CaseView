f002e2c02f05ffea410579e3d6b300823b5db5ee
Akshay <otainfo@otainfo.com>
2021-05-09 23:14:21 -0700

    install.sh for making the environment

---
+++ install.sh
@@ -0,0 +1,21 @@
+sudo apt-get update
+sudo apt-get install libmysqlclient-dev
+sudo apt-get install python-dev
+sudo pip install python-dateutil==2.8.0
+sudo pip install Django==1.8.4
+sudo pip install djangorestframework=3.3.0
+sudo pip install pygments
+sudo pip install djangorestframework-jwt==1.11.0
+sudo pip install django-cors-headers==3.0.2
+sudo pip install django-sslserver
+sudo pip install django-filter==0.14.0
+sudo pip install django_smtp_ssl
+sudo pip install MySQL-python==1.2.5
+sudo pip install six==1.11.0
+sudo pip install requests
+sudo pip install PyJWT==1.7.1
+sudo pip install psycopg2==2.8.5
+sudo pip install simplejson==3.16.0
+
+
+
