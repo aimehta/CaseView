1fcc2aecd0b2e2d1595bf1acf9e63b616c67c2e5
aimehta <a@aimehta.com>
2021-05-11 20:58:01 -0700

    Updated to support Ubuntu 20.04

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 sudo apt-get update
 sudo apt install git
-sudo apt install python-pip
+sudo apt-get install curl
+sudo add-apt-repository universe
+sudo apt update
+sudo apt install python2
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+sudo python2 get-pip.py
+sudo apt-get install python-pip
 sudo apt-get install libmysqlclient-dev
 sudo apt-get install python-dev-is-python2
 sudo apt-get install python-dev
-sudo pip install python-dateutil==2.8.0
+sudo pip install psycopg2-binary
 sudo pip install Django==1.8.4
+sudo pip install Pillow
+sudo pip install python-dateutil==2.8.0
 sudo pip install djangorestframework==3.3.0
 sudo pip install pygments
 sudo pip install djangorestframework-jwt==1.11.0
@@ -13,12 +21,8 @@ sudo pip install django-cors-headers==3.0.2
 sudo pip install django-sslserver
 sudo pip install django-filter==0.14.0
 sudo pip install django_smtp_ssl
-sudo pip install MySQL-python==1.2.5
 sudo pip install six==1.11.0
 sudo pip install requests
 sudo pip install PyJWT==1.7.1
-sudo pip install psycopg2==2.8.5
 sudo pip install simplejson==3.16.0
-
-
-
+sudo pip install Django==1.8.4
